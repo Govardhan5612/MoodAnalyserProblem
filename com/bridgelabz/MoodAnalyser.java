0177e8b926ddf0cf27ea3418f7cd848bd612535e
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-24 07:19:06 +0530

    In case of null or empty mood throw custom exception

--- com/bridgelabz/MoodAnalyser.java
+++ com/bridgelabz/MoodAnalyser.java
@@ -15,38 +15,34 @@ public class MoodAnalyser {
      * create default and parameterized constructors
      */
     public MoodAnalyser(String message) {
-        if (message.equalsIgnoreCase("null")){
-            this.message=null;
-        }
-        else {
-            this.message = message;
-        }
+        this.message = message;
     }
 
     public MoodAnalyser() {
 
     }
 
-    public void moodAnalysis() {
+    public void moodAnalysis() throws MoodAnalysisExceptiom {
         /**
          * In this method return mood
          */
         try {
-
-
-            boolean status = message.toLowerCase().contains("sad");
-            if (status == true) {
-                System.out.println( "Sad");
+            if (message.length() == 0) {
+                throw new MoodAnalysisExceptiom("Enter proper message");
             } else {
-                System.out.println( "Happy");
+                boolean status = message.toLowerCase().contains("sad");
+                if (status == true) {
+                    System.out.println("Sad");
+                } else {
+                    System.out.println("Happy");
+                }
             }
-        }
-        catch (Exception exception){
-            System.out.println("Happy");
+        } catch (Exception exception) {
+            System.out.println("Happy.." + exception);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MoodAnalysisExceptiom {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter message : ");
         String message = input.nextLine();
